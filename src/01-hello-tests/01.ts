831f33cef9341308f26425991f8d1b7138a750c0
DoWhatYouL0ve <william_ross@mailfence.com>
2022-03-30 17:52:21 +0200

    first lesson, introduction to testing

---
+++ src/01-hello-tests/01.ts
@@ -0,0 +1,13 @@
+export function sum(a: number, b: number) {
+    return a + b;
+}
+
+export function mult(a: number, b: number) {
+    return a * b;
+}
+
+export function splitIntoWords(sentence: string) {
+    let result = sentence.toLowerCase().split(' ').filter(w => w !== '' && w !== '-').map(w => w.replace('!',''))
+    return result
+}
+
